9139e61a6c5038e354381c4d3edb102d459d7387
Erik Mekkes <Mekkes.Erik@gmail.com>
2018-11-21 21:27:48 +0100

    Fix format for sensor_loop code

--- codeGeneration/src/parameter_ids/SBSYS_sensor_loop_param_id_32.java
+++ codeGeneration/src/parameter_ids/SBSYS_sensor_loop_param_id_32.java
@@ -7,6 +7,7 @@ public class SBSYS_sensor_loop_param_id_32 implements ParamID {
     private String defaultValue= "100000";
     // parameter name
     private String name = paramIdName.substring(0,paramIdName.length()-12);
+    private String baseIndent = "\t\t";
 
     public SBSYS_sensor_loop_param_id_32(String paramIdName, String defaultValue) {
         this.paramIdName = paramIdName;
@@ -22,20 +23,20 @@ public class SBSYS_sensor_loop_param_id_32 implements ParamID {
     }
 
     public String getterFunc() {
-        String func = "\tcase " + paramIdName + " :\n";
-        func += "\t\t*((uint32_t*)value) = mem_pool." + name + ";\n";
-        func += "\t\tcnv32_8(mem_pool." + name + ", buf);\n";
-        func += "\t\t*size = 4;\n";
-        func += "\t\tbreak;\n";
+        String func = baseIndent + "case " + paramIdName + " :\n";
+        func += baseIndent + "\t*((uint32_t*)value) = mem_pool." + name + ";\n";
+        func += baseIndent + "\tcnv32_8(mem_pool." + name + ", buf);\n";
+        func += baseIndent + "\t*size = 4;\n";
+        func += baseIndent + "\tbreak;";
         return func;
     }
 
     public String setterFunc() {
-        String func = "\tcase " + paramIdName + " :\n";
-        func += "uint8_t *buf;";
-        func += "buf = (uint8_t*)value;";
-        func += "cnv8_32LE(&buf[0], &mem_pool." + name + " );";
-        func += "break;\n";
+        String func = baseIndent + "case " + paramIdName + " :\n";
+        func += baseIndent + "uint8_t *buf;\n";
+        func += baseIndent + "buf = (uint8_t*)value;\n";
+        func += baseIndent + "cnv8_32LE(&buf[0], &mem_pool." + name + " );\n";
+        func += baseIndent + "break;";
         return func;
     }
 
